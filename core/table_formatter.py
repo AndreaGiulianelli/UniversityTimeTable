a7ce4af795d93ee32dff68aa57ac227e6aaa8bea
Riccardo Battistini <riccardo.battistini@protonmail.com>
2020-09-25 18:05:34 +0200

    Update color handling in table formatter

--- core/table_formatter.py
+++ core/table_formatter.py
@@ -1,21 +1,31 @@
 from core.subject import Subject
 from termcolor import colored
 
-def format_subject(subject):
-    row_format = "│ {:31} │ {:49} │ {:26} │"
-    subject_params = Subject.get_params(subject)
+def format_subject(subject, colors):
     
-    time = colored(subject_params.get("start_time") + "-" + subject_params.get("end_time"), 'red')
-    course = colored(subject_params.get("title"), 'blue')
-    place = colored(subject_params.get("aula_text"), 'green')
+    subject_params = Subject.get_params(subject)
+    time = subject_params.get("start_time") + "-" + subject_params.get("end_time")
+    course = subject_params.get("title")
+    place = subject_params.get("aula_text")
+
+    if colors is True:
+        time = colored(time, 'red')
+        course = colored(course, 'blue')
+        place = colored(place, 'green')
+        row_format = "│ {:31} │ {:49} │ {:26} │"
+    else:
+        row_format = "│ {:22} │ {:40} │ {:17} │"
     
     print(row_format.format(time, course, place))
 
 
-def put_table_header(current_date):
+def put_table_header(current_date, colors):
+
     date_format = "┤ {:^20} ├"
-    date_format = date_format.format(current_date.strftime('%m/%d/%Y') + f" {current_date.strftime('%A')}")
-    date_format = colored(date_format, 'cyan')
+    date_format = date_format.format(" ".join([current_date.strftime('%m/%d/%Y'), current_date.strftime('%A')]))
+
+    if colors is True:
+        date_format = colored(date_format, 'cyan')
     
     ret = """                                                                                              
                ┌──────────────────────┐                                                   
